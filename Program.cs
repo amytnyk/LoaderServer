f7be5a7a5af0f2f9a15bd69cdb9f237d09560128
amytnyk <alex.mytnyk@ukr.net>
2019-04-10 17:56:39 +0300

    Big files support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -22,6 +23,12 @@ namespace LoaderServer
 			this.data = data;
 		}
 	}
+	
+	class SaveResponse
+	{
+		public string state;
+		public bool last_chunk;
+	}
 
 	class Program
 	{
@@ -35,6 +42,31 @@ namespace LoaderServer
 		}
 
 		public static int percentage = 0;
+		public static StringBuilder sb;
+		public static string text;
+
+		/*public void Handle(IAsyncResult result)
+		{
+			HttpListener listener = (HttpListener)result.AsyncState;
+			try
+			{
+				HttpListenerContext context = listener.EndGetContext(result);
+				StreamWriter writer = new StreamWriter(context.Response.OutputStream);
+				writer.Write("<html><body>");
+				writer.Write("This call was handled in " + Thread.CurrentThread.ManagedThreadId);
+				Thread.Sleep(new TimeSpan(0, 0, 5));
+				writer.Write("<br>");
+				writer.Write("Done");
+				writer.Write("</body></html>");
+				writer.Close();
+				context.Response.OutputStream.Close();
+				context.Response.Close();
+			}
+			finally
+			{
+				listener.BeginGetContext(new AsyncCallback(Handle), listener);
+			}
+		}*/
 
 		static void Main(string[] args)
 		{
@@ -47,8 +79,20 @@ namespace LoaderServer
 			HttpListener listener = new HttpListener();
 			listener.Prefixes.Add(@"http://localhost:8000/");
 
+			StreamReader sr = new StreamReader("text.txt");
+			text = sr.ReadToEnd();
+			sr.Close();
+
 			Thread t = new Thread(add);
 			t.Start();
+
+			//while (true)
+			//{
+			//	listener.BeginGetContext(new AsyncCallback(Handle), listener);
+			//	break;
+			//}
+
+			sb = new StringBuilder();
 
 			while (true)
 			{
@@ -62,19 +106,25 @@ namespace LoaderServer
 				if (request.QueryString["action"] == "save")
 				{
 					string new_text = request.QueryString["text"];
-					Console.WriteLine("New text - " + new_text);
-					string JSONstring = JsonConvert.SerializeObject("Saved");
+					sb.Append(new_text);
+					if (request.QueryString["chunk"] == "last")
+					{
+						text = sb.ToString();
+						Console.WriteLine("New text - " + sb);
+						sb.Clear();
+					}
+					string JSONstring = JsonConvert.SerializeObject(new SaveResponse { state = "Saved", last_chunk = request.QueryString["chunk"] == "last" });
 					string JSONPstring = string.Format("{0}({1});", request.QueryString["callback"], JSONstring);
 					byte[] buffer = Encoding.UTF8.GetBytes(JSONPstring);
 					response.ContentLength64 = buffer.Length;
-					System.IO.Stream output = response.OutputStream;
+					Stream output = response.OutputStream;
 					output.Write(buffer, 0, buffer.Length);
 				}
 				else
 				{
 					Response response_obj = null;
 					if (percentage > 100)
-						response_obj = new Response("Loaded", "Loaded", "TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\nTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\nTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\nTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\nTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\nTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest\n");
+						response_obj = new Response("Loaded", "Loaded", text);
 					else if (percentage > 60)
 						response_obj = new Response("Loading", "Last preparations", Convert.ToString(percentage) + "%");
 					else if (percentage > 30)
@@ -84,12 +134,10 @@ namespace LoaderServer
 					string responseString = JsonConvert.SerializeObject(response_obj);
 					byte[] buffer = Encoding.UTF8.GetBytes(responseString);
 					response.ContentLength64 = buffer.Length;
-					System.IO.Stream output = response.OutputStream;
+					Stream output = response.OutputStream;
 					output.Write(buffer, 0, buffer.Length);
 					output.Close();
 				}
-
-				listener.Stop();
 			}
 		}
 	}
